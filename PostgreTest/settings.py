31b6054e214b00aac20a3023c2887c73a65a1f3d
NurlashKO <zh.nurlan96@gmail.com>
2016-05-25 14:16:44 +0600

    Postgre Added

--- PostgreTest/settings.py
+++ PostgreTest/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'PostgreTest.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mylocaldb',
+        'USER': 'edinhiadmin',
+        'PASSWORD': 'jw8s0F4',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
